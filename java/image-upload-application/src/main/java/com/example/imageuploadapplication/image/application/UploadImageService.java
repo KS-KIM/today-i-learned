413c6bb3145ac1ee2459aeeb6117b2d40e823770
K.S.KIM <nullable@kakao.com>
2022-01-10 21:40:24 +0900

    이미지 파일타입 검증 추가

--- java/image-upload-application/src/main/java/com/example/imageuploadapplication/image/application/UploadImageService.java
+++ java/image-upload-application/src/main/java/com/example/imageuploadapplication/image/application/UploadImageService.java
@@ -13,14 +13,24 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Service
 public class UploadImageService {
+    private static final String SUPPORT_CONTENT_TYPE = "image";
+
     private final ImageUploader imageUploader;
 
     public String uploadImage(MultipartFile file) throws IOException {
+        validateContentType(file);
         String originalFileName = Objects.requireNonNull(file.getOriginalFilename());
         String saveFileName = generateRandomFileName(originalFileName);
         InputStream inputStream = file.getInputStream();
         imageUploader.upload(new UploadImageRequest(inputStream, file.getSize(), saveFileName));
         return saveFileName;
+    }
+
+    private void validateContentType(MultipartFile file) {
+        String contentType = Objects.requireNonNull(file.getContentType(), "파일 형식을 찾을 수 없습니다.");
+        if (!contentType.startsWith(SUPPORT_CONTENT_TYPE)) {
+            throw new IllegalArgumentException("파일 형식이 올바르지 않습니다. contentType=" + file.getContentType());
+        }
     }
 
     private String generateRandomFileName(String originalFileName) {
--- java/image-upload-application/src/main/java/com/example/imageuploadapplication/image/ui/ImageController.java
+++ java/image-upload-application/src/main/java/com/example/imageuploadapplication/image/ui/ImageController.java
@@ -1,6 +1,5 @@
 package com.example.imageuploadapplication.image.ui;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URI;
 
@@ -8,14 +7,12 @@ import org.springframework.http.MediaType;
 import org.springframework.http.MediaTypeFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.example.imageuploadapplication.common.ApiErrorResponse;
 import com.example.imageuploadapplication.image.application.DownloadImageService;
 import com.example.imageuploadapplication.image.application.UploadImageService;
 
@@ -28,13 +25,7 @@ public class ImageController {
     private final UploadImageService uploadImageService;
     private final DownloadImageService downloadImageService;
 
-    @PostMapping(
-            value = "/upload",
-            consumes = {
-                    MediaType.IMAGE_JPEG_VALUE,
-                    MediaType.IMAGE_PNG_VALUE,
-                    MediaType.IMAGE_GIF_VALUE
-            })
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Void> uploadImage(MultipartFile file) throws IOException {
         String fileName = uploadImageService.uploadImage(file);
         return ResponseEntity.created(URI.create(fileName)).build();
@@ -46,15 +37,5 @@ public class ImageController {
         MediaType mediaType = MediaTypeFactory.getMediaType(fileName)
                 .orElseThrow(() -> new IllegalArgumentException("올바르지 않은 파일 형식입니다."));
         return ResponseEntity.ok().contentType(mediaType).body(imageFile);
-    }
-
-    @ExceptionHandler(value = FileNotFoundException.class)
-    public ResponseEntity<ApiErrorResponse> handleFileNotFoundException(FileNotFoundException exception) {
-        return ResponseEntity.notFound().build();
-    }
-
-    @ExceptionHandler(value = IOException.class)
-    public ResponseEntity<ApiErrorResponse> handleIOException(IOException exception) {
-        return ResponseEntity.internalServerError().body(new ApiErrorResponse("파일을 처리하는 중 오류가 발생했습니다."));
     }
 }
