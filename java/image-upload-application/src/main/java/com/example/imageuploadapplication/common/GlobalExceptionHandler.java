41dd8b515d471eddfb089dedcb92c63b3bce255d
K.S.KIM <nullable@kakao.com>
2022-01-10 21:40:37 +0900

    전역 예외처리 추가

---
+++ java/image-upload-application/src/main/java/com/example/imageuploadapplication/common/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.example.imageuploadapplication.common;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ControllerAdvice
+@RestController
+public class GlobalExceptionHandler {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public ApiErrorResponse handleIllegalArgumentException(IllegalArgumentException exception) {
+        log.error(exception.getMessage());
+        return new ApiErrorResponse("요청이 올바르지 않습니다.");
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(value = FileNotFoundException.class)
+    public ApiErrorResponse handleFileNotFoundException(FileNotFoundException exception) {
+        log.error(exception.getMessage());
+        return new ApiErrorResponse("파일을 찾을 수 없습니다.");
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(value = IOException.class)
+    public ApiErrorResponse handleIOException(IOException exception) {
+        log.error(exception.getMessage());
+        return new ApiErrorResponse("파일을 처리하는 중 오류가 발생했습니다.");
+    }
+}
